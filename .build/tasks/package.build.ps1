cd3670601af6988becb4cb978f9e306d61dc9162
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2022-09-26 18:56:10 -0400

    feat: Added local repository tasks

--- .build/tasks/package.build.ps1
+++ .build/tasks/package.build.ps1
@@ -1,7 +1,7 @@
 
 
 #synopsis: create a temporary repository named after the Project
-task register_local_repo {
+task register_project_repo {
     if ((Get-PackageSource | Select-Object -ExpandProperty Name) -notcontains $Project.Name) {
         $repo_path = Join-Path $Project.Path.Artifact $Project.Name
         if (-not(Test-Path $repo_path)) { mkdir $repo_path -Force | Out-Null }
@@ -13,7 +13,7 @@ task register_local_repo {
             <#ProviderName    = 'PowerShellGet'#>
         }
         Write-Build DarkBlue (
-            '  Registering PackageSource {0} at {1}' -f $local_repo.Name, $local_repo.Location
+            '  Registering PackageSource {0} at {1}' -f $local_repo.Name, $local_repo.PublishLocation
         )
         $register = Register-PSRepository @local_repo | Out-String
         Write-Build DarkGray $register
@@ -21,7 +21,7 @@ task register_local_repo {
 }
 
 #synopsis: unregister the temporary repo
-task unregister_local_repo {
+task unregister_project_repo {
     Write-Build DarkBlue "  Unregistering PackageSource $($Project.Name)"
     if ((Get-PackageSource | Select-Object -ExpandProperty Name) -contains $Project.Name) {
         Unregister-PackageSource -Name $Project.Name
@@ -38,5 +38,28 @@ task generate_nuget_package {
             Repository = $Project.Name
         }
         Publish-Module @options
+    }
+}
+
+#synopsis: publish module in the local system repository (psrepo == Local)
+task publish_to_local_repo {
+    assert ((Get-PackageSource | Select-Object -ExpandProperty Name) -contains 'local') "No local system repository found"
+    Write-Build DarkGray "Checking for a recent package in the Artifact directory"
+    $found =  Get-ChildItem (Join-Path $Project.Path.Artifact $Project.Name) -Filter "$($Project.Name)*.nupkg"
+    switch ($found.Count) {
+        0 {
+            foreach ($key in $Modules.Keys) {
+                Write-Build DarkBlue "  Publishing $key to local system repository"
+                $options = @{
+                    Path       = $Modules[$key].Staging
+                    Repository = 'Local'
+                }
+                Publish-Module @options
+            }
+        }
+        default {
+            $dest = Get-PSRepository Local | Select-Object -ExpandProperty PublishLocation
+            $found | Copy-Item -Destination $dest
+        }
     }
 }
