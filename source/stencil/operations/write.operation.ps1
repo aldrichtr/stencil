446bb8ed3020f3bca390fb2eec0b6e260def045c
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2023-11-13 17:31:32 -0500

    feat: write messages to the console

---
+++ source/stencil/operations/write.operation.ps1
@@ -0,0 +1,40 @@
+
+Register-StencilOperation 'write' {
+    param($params)
+    if (-not ([string]::IsNullOrEmpty($params.Message))) {
+        $message = [System.Text.StringBuilder]::new()
+        $needsReset = $false
+        if ($params.ContainsKey('Foreground')) {
+            if ($null -ne $PSStyle.Foreground.($params.Foreground)) {
+                [void]$message.Append($PSStyle.Foreground.($params.Foreground))
+                $needsReset = $true
+            }
+        }
+        if ($params.ContainsKey('Background')) {
+            if ($null -ne $PSStyle.Background.($params.Background)) {
+                [void]$message.Append($PSStyle.Background.($params.Background))
+                $needsReset = $true
+            }
+        }
+        if ($params.ContainsKey('Blink')) {
+            [void]$message.Append($PSStyle.Blink)
+            $needsReset = $true
+        }
+        if ($params.ContainsKey('Bold')) {
+            [void]$message.Append($PSStyle.Bold)
+            $needsReset = $true
+        }
+        [void]$message.Append($params.Message)
+
+        if ($needsReset) {
+            [void]$message.Append($PSStyle.Reset)
+        }
+
+        $options = @{
+            Tags                  = $params.Tags
+            MessageData           = $message.ToString()
+            InformationAction = 'Continue'
+        }
+        Write-Information @options
+    }
+} -Description 'Write a message to the console'
