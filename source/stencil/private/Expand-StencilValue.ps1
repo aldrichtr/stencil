53290ad93bbe663ba5b78e18d8b39a56d8542899
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2023-11-13 16:34:28 -0500

    fix: tokens are not handled for arrays

--- source/stencil/private/Expand-StencilValue.ps1
+++ source/stencil/private/Expand-StencilValue.ps1
@@ -27,6 +27,8 @@ function Expand-StencilValue {
     begin {
         Write-Debug "-- Begin $($MyInvocation.MyCommand.Name)"
         $keyNotFound = $false
+        $tokenPattern = '\$\{(?<var>.+?)\}'
+
     }
     process {
         <#------------------------------------------------------------------
@@ -37,20 +39,17 @@ function Expand-StencilValue {
             '${}' and loop over each match
           - for this match, if the match was a multi-level value (i.e. env.foo)
             loop over each level
-
-
         ------------------------------------------------------------------#>
-        if (-not($PSBoundParameters.ContainsKey('Data'))) {
-            Write-Verbose '  No Job data given, using global Jobs Table'
-            $Data = $script:Jobs # if no job config was given, use the "module's Jobs Table"
+        if (-not ($PSBoundParameters.ContainsKey('Data'))) {
+            Write-Debug '  No Job data given, using global Jobs Table'
+            throw "No data given for expanding stencil values"
         }
 
         :line foreach ($line in $Value) {
             Write-Debug "  Looking for tokens in '$line'"
             # recursively look for tokens until none are found
-            $result = $line | Select-String -Pattern '\$\{(?<var>.+?)\}' -AllMatches
 
-            while ($result.Matches.Count -gt 0) {
+            while ( ($result = $line | Select-String -Pattern $tokenPattern -AllMatches ) ) {
                 Write-Debug "   Found $($result.Matches.Count) tokens"
                 $result.Matches | ForEach-Object {
                     $found = $_.Groups[0].Value
@@ -70,60 +69,85 @@ function Expand-StencilValue {
                     $level = 0
                     :key foreach ($part in $parts) {
                         $level++
-                        Write-Debug "  Level #$level - Getting variable at '$part'"
-                        Write-Debug "   $part parent object is a $($descend.GetType())"
+                        Write-Debug "  Level #$level - looking for $part item of object [$($descend.GetType().FullName)]"
                         <#------------------------------------------------------------------
                          As we "descend" down the Data object, the part may be an object,
                          table, array, etc.  We need to get down to the "last" part to
                          find that value so that we can associate the string '${env.foo.one}'
                          with the value in $Data.env.foo.one
+                         If the part is a number , then assume that is indexing an array
                         ------------------------------------------------------------------#>
-                        switch ($descend.GetType()) {
-                            'Hashtable' {
+
+                        :descend switch ($descend) {
+                            {$_ -is [Hashtable]} {
                                 if ($descend.Keys -contains $part) {
                                     Write-Debug "  HashTable: Found hashtable '$part'"
                                     Write-Debug "   Setting current descent to $($descend[$part])"
                                     $descend = $descend[$part]
+                                    continue descend
                                 } else {
                                     Write-Verbose "Warning: Key '$part' not found"
                                     $keyNotFound = $true
                                     break key
                                 }
                             }
-                            'PSCustomObject' {
+                            {$_ -is [PSCustomObject]} {
                                 if ($descend.psobject.properties.Name -contains $part) {
                                     Write-Debug "    Object: Found object property '$part'"
                                     $descend = $descend | Select-Object -ExpandProperty $part
                                     Write-Debug "     Setting current descent to $($descend.GetType()) $descend"
+                                    continue descend
                                 } else {
                                     Write-Verbose "Warning: Key '$part' not found"
                                     $keyNotFound = $true
                                     break key
                                 }
                             }
-                            'string' {
+                            {($_ -is [Array]) -or
+                             ($_ -is [System.Collections.ArrayList])} {
+                                 if ($part -match '\d+') {
+                                     $index = [int]($Matches.1)
+                                     Write-Debug "    Array: Found array with $($descend.Count) items"
+                                     if ($index -lt $descend.Count) {
+                                        if ($null -ne $descend[$index]) {
+                                            $descend = $descend[$index]
+                                            continue descend
+                                        } else {
+                                            Write-Verbose "Warning: Key $part not found"
+                                            $keyNotFound = $true
+                                            break key
+                                        }
+                                    } else {
+                                        Write-Verbose "Warning $index is greater than index of items"
+                                        $keyNotFound = $true
+                                        break key
+                                    }
+                                }
+                            }
+                            {$_ -is [string]} {
                                 Write-Debug "  Found string value '$part'"
                                 break key
                             }
                             Default {
+                                continue descend
+
                             }
-                        } # switch
-                    } # foreach part
+                        } # end switch
+                    } # end foreach part
                     if ($keyNotFound) {
                         Write-Verbose "  $found not found in input data"
                     } else {
                         $line = $line -replace [regex]::Escape($found) , $descend
                         Write-Debug "   Replacing token '$found'. Level #$level  line is now '$line'"
                     }
-                } # end while
-                $result = $line | Select-String -Pattern '\$\{(?<var>.+?)\}' -AllMatches
-            Write-Debug "  final text after expansion '$line'"
-        }
-        # All tokens found have been replaced, output the line.  It may or may not have been changed
-        $line
+                } # end foreach result result
+                Write-Debug "  final text after expansion '$line'"
+            } # end while result
+            # All tokens found have been replaced, output the line.  It may or may not have been changed
+            $ExecutionContext.InvokeCommand.ExpandString($line) | Write-Output
+        } # end foreach line
     }
-}
-end {
-    Write-Debug "-- End $($MyInvocation.MyCommand.Name)"
-}
+    end {
+        Write-Debug "-- End $($MyInvocation.MyCommand.Name)"
+    }
 }
